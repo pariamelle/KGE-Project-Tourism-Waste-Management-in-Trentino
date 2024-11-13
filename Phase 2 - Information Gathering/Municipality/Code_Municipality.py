79ae90480dd0decb9ce5ea83977f1c66061f6bf9
pariamelle <mariaamalia.pelle@studenti.unitn.it>
2024-11-13 15:45:47 +0100

    Adding Waste production & code for Municipality

---
+++ Phase 2 - Information Gathering/Municipality/Code_Municipality.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+
+# DATA LOADING
+#gdf = gpd.read_file("Raw data/Trento_MunicipalityBorders.geojson")
+#df_population = pd.read_csv("Raw data/Pop_Trento2024.csv")
+gdf = gpd.read_file("Raw data/Trento_MunicipalityBorders.geojson")
+df_population = pd.read_csv("Raw data/Pop_Trento2024.csv")
+
+# DATA CLEANING
+# Usefull tags
+istat_geojson = "ref:ISTAT"  # ISTAT code column in GeoJSON
+istat_csv = "Codice comune"  # ISTAT code column in CSV
+population_column = "Totale"        # Column Name of Total population 
+
+# keeping only essential columns
+df_population = df_population[[istat_csv,population_column]]
+
+# Converting ISTAT codes in string for key usage
+df_population[istat_csv] = df_population[istat_csv].astype(str)
+print(gdf[istat_geojson].dtype)
+
+
+#gdf = gdf.merge(df_population[[istat_csv, population_column]],
+ #               left_on=istat_geojson,
+  #              right_on=istat_csv,
+   #             how="left")
+
+#gdf = gdf.drop(columns=[istat_csv])
+
+# Salva il file GeoJSON aggiornato con la colonna della popolazione
+#output_file = "../Municipality_Table/Municipality_final"
+
+# Save the DataFrame to a CSV file
+#gdf.to_file(output_file, driver="GeoJSON")

2843633c4d98d18dd3f77cebeda17c55b37fafba
pariamelle <mariaamalia.pelle@studenti.unitn.it>
2024-11-13 17:35:42 +0100

    final code

--- Phase 2 - Information Gathering/Municipality/Code_Municipality.py
+++ Phase 2 - Information Gathering/Municipality/Code_Municipality.py
@@ -3,10 +3,8 @@ import numpy as np
 import geopandas as gpd
 
 # DATA LOADING
-#gdf = gpd.read_file("Raw data/Trento_MunicipalityBorders.geojson")
-#df_population = pd.read_csv("Raw data/Pop_Trento2024.csv")
-gdf = gpd.read_file("C:/Users/pelle/Desktop/Magistrale secondo anno/Knowledge Graph Engineering/KGE-git/KGE-Project-Tourism-Waste-Management-in-Trentino/Phase 2 - Information Gathering/Municipality/Raw data/Trento_MunicipalityBorders.geojson")
-df_population = pd.read_csv("C:/Users/pelle/Desktop/Magistrale secondo anno/Knowledge Graph Engineering/KGE-git/KGE-Project-Tourism-Waste-Management-in-Trentino/Phase 2 - Information Gathering/Municipality/Raw data/Pop_Trento2024.csv")
+gdf = gpd.read_file("Raw data/Trento_MunicipalityBorders.geojson") # use your path 
+df_population = pd.read_csv("Raw data/Pop_Trento2024.csv") # use your path
 
 # DATA CLEANING
 # Usefull tags
@@ -19,17 +17,21 @@ df_population = df_population[[istat_csv,population_column]]
 
 # Converting ISTAT codes in string for key usage
 df_population[istat_csv] = df_population[istat_csv].astype(str)
-gdf[istat_geojson]=gdf[istat_geojson].astype(str)
+# Remove leading zeros from istat_geojson column in gdf
+gdf[istat_geojson] = gdf[istat_geojson].astype(str).str.lstrip('0')
 
-#gdf = gdf.merge(df_population[[istat_csv, population_column]],
- #               left_on=istat_geojson,
-  #              right_on=istat_csv,
-   #             how="left")
+#gdf[istat_geojson]=gdf[istat_geojson].astype(str)
 
-#gdf = gdf.drop(columns=[istat_csv])
+#Merge the two dataset
+gdf_new = gdf.merge(df_population[[istat_csv, population_column]],
+                left_on=istat_geojson,
+                right_on=istat_csv,
+                how="left")
 
-# Salva il file GeoJSON aggiornato con la colonna della popolazione
-#output_file = "../Municipality_Table/Municipality_final"
+gdf_new = gdf_new.drop(columns=[istat_csv])
 
-# Save the DataFrame to a CSV file
-#gdf.to_file(output_file, driver="GeoJSON")
+print(gdf_new[population_column])
+
+# Save the new updated file
+output_file = "Municipality_tab"
+gdf_new.to_file(output_file, driver="GeoJSON")

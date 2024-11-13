7a2f4685cc1e98333c8f750dc5b4c0aaa91fa707
pariamelle <mariaamalia.pelle@studenti.unitn.it>
2024-11-13 22:05:01 +0100

    column renamed for easier searc

--- Phase 2 - Information Gathering/Municipality/Code_Municipality.py
+++ Phase 2 - Information Gathering/Municipality/Code_Municipality.py
@@ -3,14 +3,14 @@ import numpy as np
 import geopandas as gpd
 
 # DATA LOADING
-gdf = gpd.read_file("Raw data/Trento_MunicipalityBorders.geojson") # use your path 
-df_population = pd.read_csv("Raw data/Pop_Trento2024.csv") # use your path
+gdf = gpd.read_file("C:/Users/pelle/Desktop/Magistrale secondo anno/Knowledge Graph Engineering/KGE-git/KGE-Project-Tourism-Waste-Management-in-Trentino/Phase 2 - Information Gathering/Municipality/Raw data/Trento_MunicipalityBorders.geojson") # use your path 
+df_population = pd.read_csv("C:/Users/pelle/Desktop/Magistrale secondo anno/Knowledge Graph Engineering/KGE-git/KGE-Project-Tourism-Waste-Management-in-Trentino/Phase 2 - Information Gathering/Municipality/Raw data/Pop_Trento2024.csv") # use your path
 
 # DATA CLEANING
 # Usefull tags
 istat_geojson = "ref:ISTAT"  # ISTAT code column in GeoJSON
 istat_csv = "Codice comune"  # ISTAT code column in CSV
-population_column = "Totale"        # Column Name of Total population 
+population_column = "Totale" 
 
 # keeping only essential columns
 df_population = df_population[[istat_csv,population_column]]
@@ -30,8 +30,10 @@ gdf_new = gdf.merge(df_population[[istat_csv, population_column]],
 
 gdf_new = gdf_new.drop(columns=[istat_csv])
 
-print(gdf_new[population_column])
+gdf_new.rename(columns={population_column: "Popolazione Totale"}, inplace=True)
+
+gdf_new = gdf_new.dropna(axis=1, how='all') #dealing with NaN
 
 # Save the new updated file
-output_file = "Municipality_tab"
+output_file = "Municipality"
 gdf_new.to_file(output_file, driver="GeoJSON")
